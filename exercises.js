f989d67a895ced809089fad73d7dd8d38a06b0da
Levi Shevick <levidshevick@gmail.com>
2022-05-03 14:49:04 -0400

    adds sum and multiply functions

--- exercises.js
+++ exercises.js
@@ -58,6 +58,15 @@
   // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
   // ---------------------
 
+  const sum = function(arr) {
+    let newArr = arr.reduce((a, b) => a + b, 0);
+    return newArr;
+  }
+
+  const  multiply = function(arr) {
+    let newArr = arr.reduce((a, b) => a * b, 1);
+    return newArr;  
+  }
 
 
   // ---------------------
